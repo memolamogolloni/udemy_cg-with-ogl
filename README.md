df39d7e26d7451265ede113fafe734dd2f712ae8
anxo <anxo.babio@fullsix.com>
2019-04-28 23:48:02 +0200

    Create shaders and pass vertex data

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
 # PRACTICES ON UDEMY'S [OPENGL COURSE](https://www.udemy.com/graphics-with-modern-opengl/)
 
-**Requires:**
-**[Conan package manager](https://conan.io/)**
+#### Requires:
+
+* **[CMake](https://cmake.org/)**
+* **[Conan Package Manager](https://conan.io/)**
 
 ## USAGE:
 
 * Create build folder and go into it:
-`mkdir build && cd /build`
+  *`mkdir build && cd /build`*
+* Install *conan* dependencies listed in *conantfile.txt*:
+  `conan install ..`
 
-* Install conan dependencies listed in *conantfile.txt*:
-`conan install ..`
-
-* Run CMake based on rules specified at *CMakeLists.txt*:
-`cmake ..`
-
+* Run CMake based on rules specified at  *CMakeLists.txt*:
+  `cmake ..`
 * Build and run:
-`make && make run`
-
+  `make && make run`
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,145 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 // Window dimensions
-const GLint WIDTH = 800, HEIGHT = 600;
+const GLint WIDTH = 800;
+const GLint HEIGHT = 600;
+
+GLuint VAO;
+GLuint VBO;
+GLuint shader;
+
+// Vertex shader
+static const char *vShader = "\
+#version 330																									\n\
+																															\n\
+layout (location = 0) in vec3 pos;														\n\
+																															\n\
+void main() {																									\n\
+	gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);								\n\
+}";
+
+// Fragment shader
+static const char *fShader = "\
+#version 130																									\n\
+																															\n\
+out vec4 colour;																							\n\
+																															\n\
+void main() {																									\n\
+	colour = vec4(1.0, 0.0, 0.0, 1.0);													\n\
+}";
+
+void addShader(GLuint programId, const char* shaderSource, GLenum shaderType) {
+	GLuint shader = glCreateShader(shaderType);
+
+	const GLchar *shaderSrc[1];
+	shaderSrc[0] = shaderSource;
+
+	GLint codeLength[1];
+	codeLength[0] = strlen(shaderSource);
+
+	// Set shader source. Arguments:
+	//	* Shader id
+	//	* Files count
+	//	* Shader source code
+	//	* Length of the source code
+	glShaderSource(shader, 1, shaderSrc, codeLength);
+	// Compile shader
+	glCompileShader(shader);
+
+		// Reserve variables to save state and errors log from shaders compiling
+	GLint result = 0;
+	GLchar eLog[1024] = { 0 };
+
+	// Link program to the GPU and check linker errors
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+	// If failed when compiling, print logging data
+	if (!result) {
+		glGetShaderInfoLog(shader, sizeof(eLog), NULL, eLog);
+		printf("Error compiling the %d shader: '%s'\n", shaderType, eLog);
+		return;
+	}
+
+	// Attach shader
+	glAttachShader(programId, shader);
+}
+
+// Compile shaders and check for errors
+void compileShaders() {
+	shader = glCreateProgram();
+
+	if (!shader) {
+		printf("Error creating the shader program!\n");
+		return;
+	}
+
+	addShader(shader, vShader, GL_VERTEX_SHADER);
+	addShader(shader, fShader, GL_FRAGMENT_SHADER);
+
+	// Reserve variables to save state and error logs from linking and validating shaders
+	GLint result = 0;
+	GLchar eLog[1024] = { 0 };
+
+	// Link program to the GPU and check linker errors
+	glLinkProgram(shader);
+	glGetProgramiv(shader, GL_LINK_STATUS, &result);
+	// If failed when compiling, print logging data
+	if (!result) {
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		printf("Error linking program: '%s'\n", eLog);
+		return;
+	}
+
+	// Validate program and check validation errors
+	glValidateProgram(shader);
+	glGetProgramiv(shader, GL_LINK_STATUS, &result);
+	// If validation failed, print logging data
+	if (!result) {
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		printf("Error validating program: '%s'\n", eLog);
+		return;
+	}
+
+	glValidateProgram(shader);
+};
+
+
+void createTriangle() {
+	GLfloat vertices[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f
+	};
+
+	// Generate and bind VAO
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+		// Generate and bind, VBO
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		// Set buffer data. Arguments:
+		//	* purpose of the buffer data. (GL_ARRAY_BUFFER -> vertex attributes)
+		//	* size of the data
+		//	* data
+		//	* usage (STATIC -> not modified frequently)
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		// Link buffer to vertex attributes at position 0. Arguments:
+		//	* location of attribute in layout
+		//	* size of group of values
+		//	* type of the values
+		//	* normalize values?
+		//	* stride (used when passing many group of values in same buffer)
+		//	* stating point of the group (also used to pass many groups to buffer)
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		// Enable vertex attributes at position 0
+		glEnableVertexAttribArray(0);
+	// Unbind VAO
+	glBindVertexArray(0);
+
+};
 
 int main() {
 	if (!glfwInit()) {
@@ -24,8 +159,7 @@ int main() {
 
 	// Create window setting widthm height and title
 	GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
-	if (!mainWindow)
-	{
+	if (!mainWindow) {
 		printf("GLFW window creation failed!");
 		glfwTerminate();
 		return 1;
@@ -41,8 +175,7 @@ int main() {
 	// Set experimental features on
 	glewExperimental = GL_TRUE;
 
-	if (glewInit() != GLEW_OK)
-	{
+	if (glewInit() != GLEW_OK) {
 		printf("GLEW initialisation failed!");
 		glfwDestroyWindow(mainWindow);
 		glfwTerminate();
@@ -52,15 +185,30 @@ int main() {
 	// Create viewport
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
-	/* Main app loop */
-	while (!glfwWindowShouldClose(mainWindow))
-	{
+
+	/* MAIN APP LOOP */
+	createTriangle();
+	compileShaders();
+
+	while (!glfwWindowShouldClose(mainWindow)) {
 		// Get user input
 		glfwPollEvents();
 
 		// RGBA clear color
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Enable the shader
+		glUseProgram(shader);
+			// Bind VAO
+			glBindVertexArray(VAO);
+				// Draw array in VAO. Arguments:
+				//	* Drawing mode
+				//	* Offset
+				//	* Size of the array
+				glDrawArrays(GL_TRIANGLES, 0, 3);
+			glBindVertexArray(0);
+		glUseProgram(0);
 
 		glfwSwapBuffers(mainWindow);
 	}
