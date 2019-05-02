829fa667a61de03896d1d3eab22272176a7abe5f
anxo <anxo.babio@fullsix.com>
2019-05-02 21:32:10 +0200

    Interpolation

--- src/main.cpp
+++ src/main.cpp
@@ -35,21 +35,24 @@ static const char *vShader = "\
 #version 330																												\n\
 																																		\n\
 layout (location = 0) in vec3 pos;																	\n\
+out vec4 vCol;																											\n\
 																																		\n\
 uniform mat4 model;																									\n\
 																																		\n\
 void main() {																												\n\
 	gl_Position = model * vec4(pos.x, pos.y, pos.z, 1.0);							\n\
+	vCol = vec4((pos + 1) / 2.0, 1.0);																\n\
 }";
 
 // Fragment shader
 static const char *fShader = "\
 #version 130																												\n\
 																																		\n\
+in vec4 vCol;																												\n\
 out vec4 colour;																										\n\
 																																		\n\
 void main() {																												\n\
-	colour = vec4(1.0, 0.0, 0.0, 1.0);																\n\
+	colour = vCol;																										\n\
 }";
 
 void addShader(GLuint programId, const char* shaderSource, GLenum shaderType) {
